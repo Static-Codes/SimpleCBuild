efff7de2028e6e91c7863fd48e17e77b7fa65637
Static-Codes <210101922+Static-Codes@users.noreply.github.com>
2026-03-13 00:28:55 -0400

    Added RuntimeIdentifer enum

---
+++ src/EasyDockerFile/Core/Types/BuildTypes/Base/RuntimeIdentifiers.cs
@@ -0,0 +1,14 @@
+namespace EasyDockerFile.Core.Types.BuildTypes.Base;
+
+public enum RuntimeIdentifier 
+{
+    linux_arm64 = 0,
+    linux_x64 = 1,
+    linux_arm = 2,
+    // Add other architectures for linux
+    osx_arm64 = 3,
+    osx_x64 = 4,
+    windows_arm64 = 5,
+    windows_x64 = 6,
+
+}
