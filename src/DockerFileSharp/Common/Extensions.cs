38af4c231cdf4751f3511956889d39a51289bc2e
Static <210101922+Static-Codes@users.noreply.github.com>
2026-03-18 22:41:14 -0400

    Created an extension method for List<IDockerInstruction> which will return the concatenated contents of each instruction in the List

--- src/DockerFileSharp/Common/Extensions.cs
+++ src/DockerFileSharp/Common/Extensions.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace DockerFileSharp.Common;
 
@@ -6,10 +7,22 @@ public static class Extensions
 {
     public static bool IsEmpty(this string? value) => string.IsNullOrWhiteSpace(value);
 
+    /// <summary>
+    /// Extension method used to map an architecture string to the corresponding System.Runtime.InteropServices.Architecture member.
+    /// </summary>
     public static Architecture ToArchitecture(this string archString) => archString switch {
         "x64" => Architecture.X64,
         "arm64" => Architecture.Arm64,
         "armhf" or "armel" => Architecture.Arm,
         _ => throw new InvalidOperationException($"Unable to parse '{archString}' to System.Runtime.InteropServices.Architecture")
     };
+
+    /// <summary>
+    /// Extension method used to create a new line seperated string based on the provided list of instructions.
+    /// </summary>
+    public static string Build(this List<IDockerInstruction> instructions) {
+        var builder = new StringBuilder();
+        instructions.ForEach(i => builder.AppendLine(i.Build()));
+        return builder.ToString();
+    }
 }
