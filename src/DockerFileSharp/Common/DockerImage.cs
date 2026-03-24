8e2670fc38f784eaad7838f86381f7c1663232cc
Static <210101922+Static-Codes@users.noreply.github.com>
2026-03-24 01:32:24 -0400

    Added rough logic for retrieving the required packages for the specified build system.

--- src/DockerFileSharp/Common/DockerImage.cs
+++ src/DockerFileSharp/Common/DockerImage.cs
@@ -1,5 +1,6 @@
 using DockerFileSharp.Common.Image;
 using DockerFileSharp.Instructions;
+using Global.Build;
 using static Global.Logging;
 
 namespace DockerFileSharp.Common;
@@ -18,9 +19,30 @@ public class DockerImage(IsoImage selectedImage)
         return $"{baseName}:{version}";
     }
 
-    public List<IDockerInstruction> GetInstructions(string? repoLink, string? repoName, List<string>? buildCommands = null, string? exportPath = null) 
+    /// <summary> 
+    ///     Builds a List of IDockerInstruction(s) 
+    /// </summary>
+    /// 
+    /// <param name="repoLink">
+    ///     A link to the repository which will be cloned within the container 
+    /// </param>
+    /// 
+    /// <param name="repoName">
+    ///     <br/> The name of the repository which will be cloned. 
+    ///     <br/> This is used in the command:
+    ///     <br/> cd "repoName"
+    /// </param>
+    /// 
+    /// <param name="installation">
+    ///     The BuildSystemInstallation object containing the installation instructions for the build system.
+    /// </param>
+    /// 
+    /// <param name="exportPath">
+    ///     <br/> The path on the container to a directory that will be extracted after compilation finishes. 
+    ///     <br/> Defaults to the root directory (will copy the entire container which will be 4.7GB, this is not recommended.)
+    /// </param>
+    public List<IDockerInstruction> GetInstructions(string? repoLink, string? repoName, BuildSystemInstallation installation, string? exportPath = "/", List<string>? buildCommands = null) 
     {
-
         if (repoName == null) {
             WriteWarningMessage("Unable to return the dockerfile instructions that are required to continue.");
             WriteErrorMessage("repoName is null in DockerImage.GetInstructions()");
@@ -37,8 +59,6 @@ public class DockerImage(IsoImage selectedImage)
             new FromInstruction(ImageName, Alias: "build")
         ];
 
-        
-
         if (ImageName.StartsWith("debian:")) 
         {
             Instructions.AddRange(  
@@ -53,6 +73,7 @@ public class DockerImage(IsoImage selectedImage)
                     "apt-get update",
                     "apt-get install git curl -y",
                     "mkdir -p /root/repos",
+
                 ]),
             ]);
         }
@@ -76,6 +97,10 @@ public class DockerImage(IsoImage selectedImage)
             return [];
         }
 
+        // Adding required packages for the build system itself.
+        Instructions.Add(new RunInstruction(GetPackagesForBuildSystem(installation)));
+
+        // Cloning and working directory instructions
         Instructions.AddRange(
         [
             new WorkDirInstruction("/root/repos"),
@@ -85,6 +110,7 @@ public class DockerImage(IsoImage selectedImage)
             new WorkDirInstruction($"/root/repos/{repoName}")
         ]);
 
+        // Handling build commands.
         if (buildCommands != null && buildCommands.Count > 0)
         {
             Instructions.Add(
@@ -94,6 +120,8 @@ public class DockerImage(IsoImage selectedImage)
             );
         }
 
+
+        // Copying the build artifacts from container to the host machine.
         if (!string.IsNullOrEmpty(exportPath))
         {
             Instructions.AddRange([
@@ -103,5 +131,28 @@ public class DockerImage(IsoImage selectedImage)
         }
 
         return Instructions;
+    }
+
+    private string[] GetPackagesForBuildSystem(BuildSystemInstallation installation) 
+    {
+        var distroName = ImageName.Split(':')[0].ToLower();
+        
+        switch (distroName) 
+		{
+			case "debian": 
+				if (installation.Debian != null && installation.Debian.InstallationCommands.Count > 0) {
+					return [.. installation.Debian.InstallationCommands];
+				}
+				return ["apt-get install -y build-essential cmake"];
+
+			case "fedora": 
+				if (installation.Fedora != null && installation.Fedora.InstallationCommands.Count > 0) {
+					return [.. installation.Fedora.InstallationCommands];
+				}
+				return ["dnf install -y @development-tools cmake"];
+
+			default: 
+				throw new InvalidOperationException("Invalid distroName passed to GetPackagesForBuildSystem()");
+		}
     }
 }
