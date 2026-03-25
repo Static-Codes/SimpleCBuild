9485a4910b784cd5cf07278366b0951da442883d
Static <210101922+Static-Codes@users.noreply.github.com>
2026-03-25 00:23:51 -0400

    Rename BuildSystemInstallation to BuildSystemInstallations

--- src/Global/Build/BuildSystemInstallations.cs
+++ src/Global/Build/BuildSystemInstallations.cs
@@ -1,10 +1,11 @@
+using System.Text;
 using System.Xml.Linq;
 using System.Xml.Serialization;
 
 namespace Global.Build;
 
 [XmlRoot(ElementName="installation")]
-public class BuildSystemInstallation { 
+public class BuildSystemInstallations { 
 
 	[XmlElement(ElementName="debian")] 
 	public DebianBuildSystemInstallation? Debian { get; set; } = null; 
@@ -19,31 +20,48 @@ public class BuildSystemInstallation {
     /// <param name="xElement">
     ///     The XElement object to be used in the creation of the BuildFiles object.
     /// </param>
-    public static BuildSystemInstallation FromXElement(XElement xElement)
+    public static BuildSystemInstallations FromXElement(XElement xElement)
     {
         if (xElement == null) {
-            return new BuildSystemInstallation();
+            return new BuildSystemInstallations();
         }
 
 		var debianBlock = xElement.Elements("debian");
 		var fedoraBlock = xElement.Elements("fedora");
 		
-        return new BuildSystemInstallation
+        return new BuildSystemInstallations
         {
 			Debian = new DebianBuildSystemInstallation() {
-				InstallationCommands = [.. GetCommands(debianBlock)],
+				InstallationCommands = [.. GetInstallationCommands(debianBlock)],
 			},
             Fedora = new FedoraBuildSystemInstallation() {
-				InstallationCommands = [.. GetCommands(fedoraBlock)],
+				InstallationCommands = [.. GetInstallationCommands(fedoraBlock)],
 			}
         };
     }
 	
-	private static IEnumerable<string> GetCommands(IEnumerable<XElement> block) 
+	private static IEnumerable<string> GetInstallationCommands(IEnumerable<XElement> block) 
 	{
 		return block
 			.Elements("installation_command")
 			.Select(cmd => cmd.Value);
+	}
+
+	public override string ToString() 
+	{
+		var debianCommands = Debian?.InstallationCommands ?? [];
+		var fedoraCommands = Fedora?.InstallationCommands ?? [];
+		
+		var builder = new StringBuilder();
+		foreach (var command in debianCommands){
+			builder.AppendLine(command);	
+		}
+
+		foreach (var command in fedoraCommands) {
+			builder.AppendLine(command);
+		}
+
+		return builder.ToString();
 	}
 }
 
