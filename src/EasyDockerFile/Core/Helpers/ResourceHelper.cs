a06eb233679a99598f9669162c36e480a2f55d26
Static <210101922+Static-Codes@users.noreply.github.com>
2026-03-28 15:25:38 -0400

    Created ResourceHelper to handle embedded resource management (specifically python files)

---
+++ src/EasyDockerFile/Core/Helpers/ResourceHelper.cs
@@ -0,0 +1,88 @@
+using EasyDockerFile.Core.API.PackageSearch;
+using EasyDockerFile.Core.Common;
+using System.Runtime.Versioning;
+using static Global.Constants;
+using static Global.Logging;
+
+namespace EasyDockerFile.Core.Helpers;
+
+public class ResourceHelper 
+{
+    private static string GenerateFileName(bool isPythonFile) 
+    {
+        var fileExtension = isPythonFile ? "py" : "tmp";
+        var fileName = $"simplecbuild-{Guid.NewGuid()}.{fileExtension}";
+        return Path.Combine(Path.GetTempPath(), fileName);
+    }
+
+
+    /// <summary>
+    /// Fetches the resource at the specified path, writes the stream to disk, and returns the temporary filepath.
+    /// </summary>
+    
+    public static string WriteEmbeddedResourceToDisk(string resourcePattern, bool isExecutable = false, bool isPythonFile = false) 
+    {
+        string? fileName = null;
+
+        Stream? resourceStream = null;
+        
+        try 
+        { 
+            resourceStream = _assembly.GetManifestResourceStream(resourcePattern);
+
+            if (resourceStream == null) {
+                WriteWarningMessage($"Unable to write resource: '{resourcePattern}' to disk.");
+                WriteErrorMessage("Variable 'resourceStream' is null.", exitCode: 1, exit: true);
+            }
+            
+            fileName = GenerateFileName(isPythonFile);
+
+            // Ensuring the file doesn't already exist prior to continuing.
+            while (File.Exists(fileName)) {
+                fileName = GenerateFileName(isPythonFile);
+            }
+
+            using var fileStream = new FileStream(fileName, FileMode.CreateNew);
+            resourceStream.CopyTo(fileStream);
+
+            if (!isExecutable) {
+                return fileName;
+            }
+            
+            bool execPermSet = false;
+
+            if (Platform.IsWindows) {
+                return Path.Combine(TEMP_DIR, fileName);
+            }
+
+            // Rosyln sees the [UnsupportedOSPlatform("windows")] attribute from this function but not the logic surrounding it's call.
+            // As such, the code below suppressed the CA1416 warning.
+            
+            #pragma warning disable 
+            execPermSet = UnixFilePermissions.SetExecutablePermissions(fileName);
+            #pragma warning restore
+
+            if (!execPermSet) {
+                WriteWarningMessage($"Unable to set executable permissions for temporary utility file: '{fileName}'");
+                WriteErrorMessage("execPermSet is false in WriteEmbeddedResourceToDisk()", exitCode: 1, exit: true);
+            } 
+
+            WriteSuccessMessage($"Executable permissions set for temporary utility file: '{fileName}'");
+        }
+
+        catch (Exception ex) 
+        {
+            WriteWarningMessage($"Unable to write resource: '{resourcePattern}' to disk.");
+            WriteErrorMessage(ex.Message, exitCode: 1, exit: true);
+        }
+
+        finally 
+        {
+            if (resourceStream != null && resourceStream.CanTimeout) {
+                resourceStream.Dispose();
+            }
+        }
+
+        return fileName;
+    }
+}
