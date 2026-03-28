c8c78302c3570479ab25ba51f6f155b357696fca
Static <210101922+Static-Codes@users.noreply.github.com>
2026-03-28 16:46:43 -0400

    Converted members of AutotoolsResources to nullables as per the outlined explanation

--- src/EasyDockerFile/Core/Types/Conversion/AutotoolsResources.cs
+++ src/EasyDockerFile/Core/Types/Conversion/AutotoolsResources.cs
@@ -3,6 +3,9 @@ namespace EasyDockerFile.Core.Types.Conversion;
 
 public class AutotoolsResources 
 {
-    public required string Auto2CMakePath { get; set; }
-    public required string CMakeInspectPath { get; set; }
+    // These two members are declared as nullable but, they will never be null at initialization.
+    // When one of these temporary files is deleted, the member is marked as null.
+    // The app will differentiate between a used and unused file, without additional boolean(s) by performing a simple null check.
+    public required string? Auto2CMakePath { get; set; }
+    public required string? CMakeInspectPath { get; set; }
 }
