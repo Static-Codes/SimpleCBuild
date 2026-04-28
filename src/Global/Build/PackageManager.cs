37ff205136bacee9fe5ad8cc488f5d835e64dacf
Static <210101922+Static-Codes@users.noreply.github.com>
2026-04-27 21:12:57 -0400

    Removed unused PackageManagerName members

--- src/Global/Build/PackageManager.cs
+++ src/Global/Build/PackageManager.cs
@@ -3,9 +3,7 @@ namespace Global.Build;
 
 public enum PackageManagerName 
 {
-    Buckaroo = 0, // https://buckaroo.pm/
     Conan = 1,    // https://conan.io/
     VCPkg = 2,    // https://github.com/microsoft/vcpkg
     Apt = 3,      // Used for sys deps
-    Dnf = 4       // Used for sys deps
 }
